33a1c28a77a84a125bd9ec67e4707369ceba2f2c
Sofiyuga2 <156468942+Sofiyuga2@users.noreply.github.com>
2024-02-03 14:58:48 +0300

    Delete application directory

--- application/internal/models/checking_in_archiveRequest.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type CheckArchiveRequest struct {
-	AccessToken      int64 `json:"access_key"`
-	Checking_archive int   `json:"verify"`
-}
--- application/internal/models/request.go
+++
@@ -1,5 +0,0 @@
-package models
-
-type Request struct {
-	DiscoveriesId int64 `json:"discoveries_id"`
-}
